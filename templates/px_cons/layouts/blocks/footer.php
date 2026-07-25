3b57717a68faf56832b4aec948b5443a3bc7e75c
Trung Chu <cssyeah@gmail.com>
2026-07-25 07:19:58 +0700

    Update style
    Update style

--- templates/px_cons/layouts/blocks/footer.php
+++ templates/px_cons/layouts/blocks/footer.php
@@ -17,69 +17,70 @@ use Joomla\Plugin\System\Pixel\Element\PixelScrollTopElement;
 // No direct access.
 defined('_JEXEC') or die('Restricted access');
 $doc = Factory::getDocument();
-$show_back_to_top = $doc->params->get('show_back_to_top','');
+$show_back_to_top = $doc->params->get('show_back_to_top', '');
 ?>
 
-<?php if($doc->countModules('footnav-1') || $doc->countModules('footnav-2') || $doc->countModules('footnav-3') || $doc->countModules('footnav-4') || $doc->countModules('footnav-5') || $doc->countModules('footnav-6')) : ?>
-<div id="px-footer-nav">
-  <div class="container">
-    <div class="row d-flex">
-    <?php if($doc->countModules('footnav-1')) : ?>
-      <div class="col-xs-6 col-sm-6 col-md-6 col-lg footnav-mod">
-        <jdoc:include type="modules" name="footnav-1" style="px_style"  />
-      </div>
-    <?php endif; ?>
+<?php if ($doc->countModules('footnav-1') || $doc->countModules('footnav-2') || $doc->countModules('footnav-3') || $doc->countModules('footnav-4') || $doc->countModules('footnav-5') || $doc->countModules('footnav-6')) : ?>
+  <div id="px-footer-nav">
+    <div class="container">
+      <div class="row d-flex">
+        <?php if ($doc->countModules('footnav-1')) : ?>
+          <div class="col-xs-6 col-sm-6 col-md-6 col-lg footnav-mod">
+            <jdoc:include type="modules" name="footnav-1" style="px_style" />
+          </div>
+        <?php endif; ?>
 
-    <?php if($doc->countModules('footnav-2')) : ?>
-      <div class="col-6 col-sm-6 col-md-6 col-lg footnav-mod">
-        <jdoc:include type="modules" name="footnav-2" style="px_style"  />
-      </div>
-    <?php endif; ?>
+        <?php if ($doc->countModules('footnav-2')) : ?>
+          <div class="col-6 col-sm-6 col-md-6 col-lg footnav-mod">
+            <jdoc:include type="modules" name="footnav-2" style="px_style" />
+          </div>
+        <?php endif; ?>
 
-    <?php if($doc->countModules('footnav-3')) : ?>
-      <div class="col-6 col-sm-6 col-md-6 col-lg footnav-mod">
-        <jdoc:include type="modules" name="footnav-3" style="px_style"  />
-      </div>
-    <?php endif; ?>
+        <?php if ($doc->countModules('footnav-3')) : ?>
+          <div class="col-6 col-sm-6 col-md-6 col-lg footnav-mod">
+            <jdoc:include type="modules" name="footnav-3" style="px_style" />
+          </div>
+        <?php endif; ?>
 
-    <?php if($doc->countModules('footnav-4')) : ?>
-      <div class="col-6 col-sm-6 col-md-6 col-lg footnav-mod">
-        <jdoc:include type="modules" name="footnav-4" style="px_style"  />
-      </div>
-    <?php endif; ?>
+        <?php if ($doc->countModules('footnav-4')) : ?>
+          <div class="col-6 col-sm-6 col-md-6 col-lg footnav-mod">
+            <jdoc:include type="modules" name="footnav-4" style="px_style" />
+          </div>
+        <?php endif; ?>
 
-    <?php if($doc->countModules('footnav-5')) : ?>
-      <div class="col-6 col-sm-6 col-md-6 col-lg footnav-mod">
-        <jdoc:include type="modules" name="footnav-5" style="px_style"  />
-      </div>
-    <?php endif; ?>
+        <?php if ($doc->countModules('footnav-5')) : ?>
+          <div class="col-6 col-sm-6 col-md-6 col-lg footnav-mod">
+            <jdoc:include type="modules" name="footnav-5" style="px_style" />
+          </div>
+        <?php endif; ?>
 
-    <?php if($doc->countModules('footnav-6')) : ?>
-      <div class="col-6 col-sm-6 col-md-6 col-lg footnav-mod">
-        <jdoc:include type="modules" name="footnav-6" style="px_style"  />
+        <?php if ($doc->countModules('footnav-6')) : ?>
+          <div class="col-6 col-sm-6 col-md-6 col-lg footnav-mod">
+            <jdoc:include type="modules" name="footnav-6" style="px_style" />
+          </div>
+        <?php endif; ?>
       </div>
-    <?php endif; ?>
     </div>
   </div>
-</div>
 <?php endif; ?>
 
-<footer id="px-footer">
+<footer id="px-footer" class="px-footer">
   <div class="container">
-    <div class="row d-flex">
-      <?php if($doc->countModules('footer')) : ?>
-      <section id="footer" class="col-sm-12 col-md-12 col-lg-12">
-        <jdoc:include type="modules" name="footer" style="none" modnum="footer"  />
-      </section>
+    <div class="px-footer-inner">
+      <?php if ($doc->countModules('footer')) : ?>
+        <div class="px-footer-l">
+          <jdoc:include type="modules" name="footer" style="none" />
+        </div>
       <?php endif; ?>
+
+      <?php if ($doc->countModules('footer-r')) : ?>
+        <div class="px-footer-r">
+          <jdoc:include type="modules" name="footer-r" style="none" />
+        </div>
+      <?php endif ?>
     </div>
 
-    <?php if($doc->countModules('lang')) : ?>
-    <div id="px-lang" class="clearfix">
-      <jdoc:include type="modules" name="lang" style="none" />
-    </div>
-    <?php endif; ?>
-
-    <?php if($show_back_to_top): echo PixelScrollTopElement::render(); endif; ?>
+    <?php if ($show_back_to_top): echo PixelScrollTopElement::render();
+    endif; ?>
   </div>
 </footer>
--- templates/px_cons/layouts/blocks/header.php
+++ templates/px_cons/layouts/blocks/header.php
@@ -19,54 +19,56 @@ defined('_JEXEC') or die;
 
 $doc = Factory::getDocument();
 $params = $doc->params;
-$menu_fixed = $params->get('menu_fixed','0');
-$show_dark_light_mode = $params->get('show_dark_light_mode','0');
+$menu_fixed = $params->get('menu_fixed', '0');
+$show_dark_light_mode = $params->get('show_dark_light_mode', '0');
 ?>
 
 <!-- TOPBAR -->
-<?php if($doc->countModules('topbar')): ?>
-<div id="px-topbar" class="px-topbar">
-  <div class="container">
-    <?php if($doc->countModules('topbar')): ?>
-      <jdoc:include type="modules" name="topbar" style="none" />
-    <?php endif; ?>      
+<?php if ($doc->countModules('topbar')): ?>
+  <div id="px-topbar" class="px-topbar">
+    <div class="container">
+      <?php if ($doc->countModules('topbar')): ?>
+        <jdoc:include type="modules" name="topbar" style="none" />
+      <?php endif; ?>
+    </div>
   </div>
-</div>
 <?php endif; ?>
 <!-- // TOPBAR -->
 
 <!-- HEADER -->
 <header id="px-header" class="px-header <?php echo $menu_fixed ? 'px-fixed-top' : ''; ?>">
   <div class="container">
-    <div class="px-logo-wrap navbar-brand">
-      <?php echo PixelTemplate::renderBlock('logo') ?>
-    </div>
+    <div class="header-inner">
+      <div class="px-logo-wrap navbar-brand">
+        <?php echo PixelTemplate::renderBlock('logo') ?>
+      </div>
 
-    <?php if ($doc->countModules('mainmenu', true)) : ?>
-      <?php echo PixelMenuElement::render(); ?>
-    <?php endif; ?>
+      <?php if ($doc->countModules('mainmenu', true)) : ?>
+        <?php echo PixelMenuElement::render(); ?>
+      <?php endif; ?>
 
-    <div class="px-menu-toggle-wrap">
-      <?php echo PixelMenuElement::renderDropdownButton() ?>
-      <!-- Offcanvas -->
-      <?php 
-      if ($doc->params->get('offcanvas')) {
-        echo PixelOffcanvasElement::render(); 
-      }
-      ?>
+      <div class="px-header-r">
+        <?php if($doc->countModules('header-r')) : ?>
+          <jdoc:include type="modules" name="header-r" style="raw" />
+        <?php endif ?>
+
+        <?php echo PixelMenuElement::renderDropdownButton() ?>
+        <!-- Offcanvas -->
+        <?php
+        if ($doc->params->get('offcanvas')) {
+          echo PixelOffcanvasElement::render();
+        }
+        ?>
+      </div>
     </div>
   </div>
 </header>
 <!-- // HEADER -->
 
-<?php if($doc->countModules('header') && !PIXEL_COM_CONFIG && !PIXEL_COM_USERS): ?>
-<div id="px-header-mod" class="px-header-mod">
-    <jdoc:include type="modules" name="header" style="none" />
-</div>
-<?php endif; ?>
-
-<?php if($doc->countModules('masthead') && !PIXEL_COM_CONFIG && !PIXEL_COM_USERS): ?>
-<div id="px-masthead" class="px-masthead">
+<!-- MASTHEAD -->
+<?php if ($doc->countModules('masthead') && !PIXEL_COM_CONFIG && !PIXEL_COM_USERS): ?>
+  <div id="px-masthead" class="px-masthead">
     <jdoc:include type="modules" name="masthead" style="none" />
-</div>
+  </div>
 <?php endif; ?>
+<!-- // MASTHEAD -->
