586e9db64b99f76ec938b8b5144ef350abc73811
Trung Chu <man86nv@gmail.com>
2026-07-27 17:33:07 +0700

    Update demo content
    Update demo content

--- administrator/logs/joomla_scheduler.php
+++ administrator/logs/joomla_scheduler.php
@@ -11,3 +11,8 @@
 2026-07-24	06:42:18	INFO	Successfully finished task#03 in 3.13 (net 3.13) seconds.
 2026-07-25	14:31:53	INFO	Running task#03 'Update Notification'.
 2026-07-25	14:31:55	INFO	Successfully finished task#03 in 1.38 (net 1.38) seconds.
+2026-07-27	02:45:46	INFO	Running task#02 'Session GC'.
+2026-07-27	02:45:46	INFO	Task> SessionGC end
+2026-07-27	02:45:46	INFO	Successfully finished task#02 in 0.01 (net 0.01) seconds.
+2026-07-27	02:45:52	INFO	Running task#03 'Update Notification'.
+2026-07-27	02:45:55	INFO	Successfully finished task#03 in 2.67 (net 2.67) seconds.
--- templates/px_cons/layouts/blocks/footer.php
+++ templates/px_cons/layouts/blocks/footer.php
@@ -21,44 +21,46 @@ $show_back_to_top = $doc->params->get('show_back_to_top', '');
 ?>
 
 <?php if ($doc->countModules('footnav-1') || $doc->countModules('footnav-2') || $doc->countModules('footnav-3') || $doc->countModules('footnav-4') || $doc->countModules('footnav-5') || $doc->countModules('footnav-6')) : ?>
-  <div id="px-footer-nav">
+  <div id="px-footer-nav" class="px-footer-nav">
     <div class="container">
-      <div class="row d-flex">
-        <?php if ($doc->countModules('footnav-1')) : ?>
-          <div class="col-xs-6 col-sm-6 col-md-6 col-lg footnav-mod">
-            <jdoc:include type="modules" name="footnav-1" style="px_style" />
+      <div class="row">
+        <div class="col-lg px-footnav-l">
+          <div class="">
+            <?php if ($doc->countModules('footnav-1')) : ?>
+              <div class="col-xs-12">
+                <jdoc:include type="modules" name="footnav-1" style="px_style" />
+              </div>
+            <?php endif; ?>
           </div>
-        <?php endif; ?>
 
-        <?php if ($doc->countModules('footnav-2')) : ?>
-          <div class="col-6 col-sm-6 col-md-6 col-lg footnav-mod">
-            <jdoc:include type="modules" name="footnav-2" style="px_style" />
+          <div class="row">
+            <?php if ($doc->countModules('footnav-2')) : ?>
+              <div class="col-xs-6 col-sm-6 col-md-6 col-lg-4">
+                <jdoc:include type="modules" name="footnav-2" style="px_style" />
+              </div>
+            <?php endif; ?>
+
+            <?php if ($doc->countModules('footnav-3')) : ?>
+              <div class="col-6 col-sm-6 col-md-6 col-lg-4">
+                <jdoc:include type="modules" name="footnav-3" style="px_style" />
+              </div>
+            <?php endif; ?>
+
+            <?php if ($doc->countModules('footnav-4')) : ?>
+              <div class="col-6 col-sm-6 col-md-6 col-lg-4">
+                <jdoc:include type="modules" name="footnav-4" style="px_style" />
+              </div>
+            <?php endif; ?>
           </div>
-        <?php endif; ?>
 
-        <?php if ($doc->countModules('footnav-3')) : ?>
-          <div class="col-6 col-sm-6 col-md-6 col-lg footnav-mod">
-            <jdoc:include type="modules" name="footnav-3" style="px_style" />
-          </div>
-        <?php endif; ?>
-
-        <?php if ($doc->countModules('footnav-4')) : ?>
-          <div class="col-6 col-sm-6 col-md-6 col-lg footnav-mod">
-            <jdoc:include type="modules" name="footnav-4" style="px_style" />
-          </div>
-        <?php endif; ?>
-
+        </div>
+        
         <?php if ($doc->countModules('footnav-5')) : ?>
-          <div class="col-6 col-sm-6 col-md-6 col-lg footnav-mod">
+          <div class="col-6 col-sm-6 col-md-6 col-lg-3 px-footnav-r">
             <jdoc:include type="modules" name="footnav-5" style="px_style" />
           </div>
         <?php endif; ?>
 
-        <?php if ($doc->countModules('footnav-6')) : ?>
-          <div class="col-6 col-sm-6 col-md-6 col-lg footnav-mod">
-            <jdoc:include type="modules" name="footnav-6" style="px_style" />
-          </div>
-        <?php endif; ?>
       </div>
     </div>
   </div>
