c880b36137e6bf807464387d497978bc323c1229
Trung Chu <man86nv@gmail.com>
2026-08-22 17:59:53 +0700

    Improve client slide
    Improve client slide

--- administrator/logs/joomla_scheduler.php
+++ administrator/logs/joomla_scheduler.php
@@ -1,53 +1,7 @@
 #
 #<?php die('Forbidden.'); ?>
-#Date: 2026-07-24 06:37:15 UTC
+#Date: 2026-08-22 09:49:50 UTC
 #Software: Joomla! 6.1.2 Stable [ Nyota ] 7-July-2026 16:00 UTC
 
 #Fields: date	time	priority	message
-2026-07-24	06:37:15	INFO	Running task#02 'Session GC'.
-2026-07-24	06:37:15	INFO	Task> SessionGC end
-2026-07-24	06:37:16	INFO	Successfully finished task#02 in 0.01 (net 0.01) seconds.
-2026-07-24	06:42:15	INFO	Running task#03 'Update Notification'.
-2026-07-24	06:42:18	INFO	Successfully finished task#03 in 3.13 (net 3.13) seconds.
-2026-07-25	14:31:53	INFO	Running task#03 'Update Notification'.
-2026-07-25	14:31:55	INFO	Successfully finished task#03 in 1.38 (net 1.38) seconds.
-2026-07-27	02:45:46	INFO	Running task#02 'Session GC'.
-2026-07-27	02:45:46	INFO	Task> SessionGC end
-2026-07-27	02:45:46	INFO	Successfully finished task#02 in 0.01 (net 0.01) seconds.
-2026-07-27	02:45:52	INFO	Running task#03 'Update Notification'.
-2026-07-27	02:45:55	INFO	Successfully finished task#03 in 2.67 (net 2.67) seconds.
-2026-07-28	02:54:10	INFO	Running task#02 'Session GC'.
-2026-07-28	02:54:10	INFO	Task> SessionGC end
-2026-07-28	02:54:10	INFO	Successfully finished task#02 in 0.01 (net 0.01) seconds.
-2026-07-28	02:54:14	INFO	Running task#03 'Update Notification'.
-2026-07-28	02:54:20	INFO	Successfully finished task#03 in 5.70 (net 5.71) seconds.
-2026-07-29	02:56:16	INFO	Running task#02 'Session GC'.
-2026-07-29	02:56:16	INFO	Task> SessionGC end
-2026-07-29	02:56:19	INFO	Successfully finished task#02 in 0.01 (net 0.01) seconds.
-2026-07-29	03:01:13	INFO	Running task#03 'Update Notification'.
-2026-07-29	03:01:16	INFO	Successfully finished task#03 in 3.06 (net 3.06) seconds.
-2026-08-05	15:53:00	INFO	Running task#02 'Session GC'.
-2026-08-05	15:53:00	INFO	Task> SessionGC end
-2026-08-05	15:53:00	INFO	Successfully finished task#02 in 0.01 (net 0.01) seconds.
-2026-08-05	22:55:41	INFO	Running task#03 'Update Notification'.
-2026-08-05	22:55:44	INFO	Successfully finished task#03 in 2.57 (net 2.57) seconds.
-2026-08-06	16:02:47	INFO	Running task#02 'Session GC'.
-2026-08-06	16:02:47	INFO	Task> SessionGC end
-2026-08-06	16:02:48	INFO	Successfully finished task#02 in 0.01 (net 0.01) seconds.
-2026-08-14	14:44:24	INFO	Running task#02 'Session GC'.
-2026-08-14	14:44:24	INFO	Task> SessionGC end
-2026-08-14	14:44:24	INFO	Successfully finished task#02 in 0.01 (net 0.01) seconds.
-2026-08-14	14:45:04	INFO	Running task#03 'Update Notification'.
-2026-08-14	14:45:07	INFO	Successfully finished task#03 in 2.06 (net 2.06) seconds.
-2026-08-18	16:13:47	INFO	Running task#02 'Session GC'.
-2026-08-18	16:13:47	INFO	Task> SessionGC end
-2026-08-18	16:13:47	INFO	Successfully finished task#02 in 0.01 (net 0.01) seconds.
-2026-08-18	16:16:51	INFO	Running task#03 'Update Notification'.
-2026-08-18	16:16:52	INFO	Task> Update Notification end
-2026-08-18	16:16:53	INFO	Successfully finished task#03 in 1.76 (net 1.77) seconds.
-2026-08-20	14:49:35	INFO	Running task#02 'Session GC'.
-2026-08-20	14:49:35	INFO	Task> SessionGC end
-2026-08-20	14:49:35	INFO	Successfully finished task#02 in 0.01 (net 0.01) seconds.
-2026-08-20	14:54:35	INFO	Running task#03 'Update Notification'.
-2026-08-20	14:54:37	INFO	Task> Update Notification end
-2026-08-20	14:54:37	INFO	Successfully finished task#03 in 2.44 (net 2.44) seconds.
+2026-08-22	09:49:50	INFO	Successfully finished task#01 in 0.02 (net 0.02) seconds.
--- templates/px_cons/flexlayout/clients-01/tmpl/default.php
+++ templates/px_cons/flexlayout/clients-01/tmpl/default.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * ------------------------------------------------------------------------
  * WT Flex Layout Module
@@ -12,28 +13,28 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-$modSubHeading        = $params->get('mod_sub_heading','');
-$modHeading           = $params->get('mod_heading','');
-$modDesc              = $params->get('mod_desc','');
+$modSubHeading        = $params->get('mod_sub_heading', '');
+$modHeading           = $params->get('mod_heading', '');
+$modDesc              = $params->get('mod_desc', '');
 $modHeadingAligment   = $params->get('mod_heading_alignment');
 $enableContainer      = $params->get('enable_container');
 
-$modBgImage           = $params->get('mod_bg_image','');
-$modBgColor           = $params->get('mod_bg_color','');
-$modTextColor         = $params->get('mod_text_color','');
-$modLinkColor         = $params->get('mod_link_color'.'');
-$modLinkHoverColor    = $params->get('mod_link_hover_color','');
-$modSpacingTop        = $params->get('mod_spacing_top','');
-$modSpacingBottom     = $params->get('mod_spacing_bot','');
+$modBgImage           = $params->get('mod_bg_image', '');
+$modBgColor           = $params->get('mod_bg_color', '');
+$modTextColor         = $params->get('mod_text_color', '');
+$modLinkColor         = $params->get('mod_link_color' . '');
+$modLinkHoverColor    = $params->get('mod_link_hover_color', '');
+$modSpacingTop        = $params->get('mod_spacing_top', '');
+$modSpacingBottom     = $params->get('mod_spacing_bot', '');
 
-$modBtnLabel = $params->get('mod_btn_label','');
-$modBtnUrl = $params->get('mod_btn_url','');
-$modBtnType = $params->get('mod_btn_type','');
+$modBtnLabel = $params->get('mod_btn_label', '');
+$modBtnUrl = $params->get('mod_btn_url', '');
+$modBtnType = $params->get('mod_btn_type', '');
 
 $modStyle = '';
-$modStyle .= $modBgImage? 'background-image: url(\''.$modBgImage.'\');' : '';
-$modStyle .= $modBgColor? ' background-color: '.$modBgColor.';' : '';
-$modStyle .= $modTextColor? ' color: '.$modTextColor.';' : '';
+$modStyle .= $modBgImage ? 'background-image: url(\'' . $modBgImage . '\');' : '';
+$modStyle .= $modBgColor ? ' background-color: ' . $modBgColor . ';' : '';
+$modStyle .= $modTextColor ? ' color: ' . $modTextColor . ';' : '';
 
 $clientsPerRow        = $styleconfig->get('clients_per_row', 6);
 $imageMaxHeight       = $styleconfig->get('image_max_height');
@@ -43,53 +44,98 @@ $items = $styleconfig->get('client_items');
 ?>
 
 <style>
-  .clients-<?php echo $module->id; ?> a {    
-    <?php echo $modLinkColor?'color:'.$modLinkColor.';':''; ?>
+  .clients-<?php echo $module->id; ?>a {
+    <?php echo $modLinkColor ? 'color:' . $modLinkColor . ';' : ''; ?>
   }
 
-  .clients-<?php echo $module->id; ?> a:hover,
-  .clients-<?php echo $module->id; ?> a:focus,
-  .clients-<?php echo $module->id; ?> a:active {
-    <?php echo $modLinkHoverColor?'color:'.$modLinkHoverColor.';':''; ?>
+  .clients-<?php echo $module->id; ?>a:hover,
+  .clients-<?php echo $module->id; ?>a:focus,
+  .clients-<?php echo $module->id; ?>a:active {
+    <?php echo $modLinkHoverColor ? 'color:' . $modLinkHoverColor . ';' : ''; ?>
   }
 </style>
 
 <div class="wt-flex-layout clients clients-<?php echo $module->id; ?> layout-01 <?php echo $modSpacingTop . ' ' . $modSpacingBottom; ?>" style="<?php echo $modStyle; ?>">
 
   <!-- Wrapping content -->
-  <?php if($enableContainer) echo '<div class="container">'; ?>
+  <?php if ($enableContainer) echo '<div class="container">'; ?>
 
-  <div class="mod-heading-wrapper clients-heading-wrapper<?php echo ' text-'. $modHeadingAligment; ?>">
-    <?php if($modSubHeading) : ?>
+  <div class="mod-heading-wrapper clients-heading-wrapper<?php echo ' text-' . $modHeadingAligment; ?>">
+    <?php if ($modSubHeading) : ?>
       <h4 class="mod-sub-heading clients-sub-heading"><?php echo $modSubHeading; ?></h4>
     <?php endif; ?>
 
-    <?php if($modHeading) : ?>
+    <?php if ($modHeading) : ?>
       <h2 class="mod-heading clients-heading"><?php echo $modHeading; ?></h2>
     <?php endif; ?>
 
-    <?php if($modDesc) : ?>
+    <?php if ($modDesc) : ?>
       <p class="mod-lead clients-lead"><?php echo $modDesc; ?></p>
     <?php endif; ?>
   </div>
 
-  <ul style="grid-template-columns: repeat(<?php echo $clientsPerRow; ?>, 1fr);">
-  <?php foreach ($items as $item) { ?>
-    <li>
-      <a href="<?php echo $item->client_link_url; ?>" title="<?php echo $item->client_name; ?>" target="_blank" style="opacity: <?php echo $imageOpacity; ?>;">
-        <img src="<?php echo $item->client_image; ?>" alt="<?php echo $item->client_name; ?>" style="max-height: <?php echo $imageMaxHeight; ?>px;" />
-      </a>
-    </li>
-  <?php } ?>
-  </ul>
-
-  <?php if($modBtnLabel) : ?>
-  <div class="mod-action client-action text-center">
-    <a href="<?php echo $modBtnUrl; ?>" title="<?php echo $modBtnLabel; ?>" class="btn <?php echo $modBtnType; ?> btn-lg"><?php echo $modBtnLabel; ?></a>
+  <div id="clients-wrapper-<?php echo $module->id; ?>" style="grid-template-columns: repeat(<?php echo $clientsPerRow; ?>, 1fr);">
+    <div class=" owl-carousel owl-theme">
+      <?php foreach ($items as $item) { ?>
+        <div class="client-item item slide slide-item">
+          <a href="<?php echo $item->client_link_url; ?>" title="<?php echo $item->client_name; ?>" target="_blank" style="opacity: <?php echo $imageOpacity; ?>;">
+            <img src="<?php echo $item->client_image; ?>" alt="<?php echo $item->client_name; ?>" style="max-height: <?php echo $imageMaxHeight; ?>px;" />
+          </a>
+        </div>
+      <?php } ?>
+    </div>
   </div>
+
+  <?php if ($modBtnLabel) : ?>
+    <div class="mod-action client-action text-center">
+      <a href="<?php echo $modBtnUrl; ?>" title="<?php echo $modBtnLabel; ?>" class="btn <?php echo $modBtnType; ?> btn-lg"><?php echo $modBtnLabel; ?></a>
+    </div>
   <?php endif ?>
 
   <!-- Wrapping content -->
-  <?php if($enableContainer) { echo '</div>';} ?>
+  <?php if ($enableContainer) {
+    echo '</div>';
+  } ?>
 
 </div>
+
+<script>
+  (function($) {
+    jQuery(document).ready(function($) {
+      var owlClientsSlide<?php echo $module->id; ?> = $("#clients-wrapper-<?php echo $module->id; ?> .owl-carousel");
+      owlClientsSlide<?php echo $module->id; ?>.owlCarousel({
+        addClassActive: true,
+        margin: 0,
+        stagePadding: 240,
+        items: 3,
+        loop: true,
+        nav: 0,
+        dotsSpeed: 400,
+        slideTransition: 'linear',
+        autoplaySpeed: 6000,
+        dots: 1,
+        autoplay: 0,
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOut',
+        autoplayTimeout: 5000,
+        smartSpeed: 1200,
+        mouseDrag: false,
+        navText: [
+          '<i class="fa-solid fa-chevron-left"></i>', // HTML for the previous button
+          '<i class="fa-solid fa-chevron-right"></i>' // HTML for the next button
+        ]
+      });
+
+      // Go to the next item
+      $('.fl-owl-next').click(function() {
+        owlClientsSlide<?php echo $module->id; ?>.trigger('next.owl.carousel');
+      })
+
+      // Go to the previous item
+      $('.fl-owl-prev').click(function() {
+        owlClientsSlide<?php echo $module->id; ?>.trigger('prev.owl.carousel');
+      })
+    });
+
+  })(jQuery);
+</script>
