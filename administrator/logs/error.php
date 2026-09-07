f04a0a024ba903515d4a12945ab2506db6ae0dfc
Trung Chu <man86nv@gmail.com>
2026-09-07 08:43:54 +0700

    Merge logs
    Merge logs

---
+++ administrator/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2026-09-03 08:29:52 UTC
+#Software: Joomla! 6.1.2 Stable [ Nyota ] 7-July-2026 16:00 UTC
+
+#Fields: datetime	priority	clientip	category	message
+2026-09-03T08:29:52+00:00	INFO	127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2026-09-03T08:30:04+00:00	INFO	127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
--- administrator/logs/joomla_scheduler.php
+++ administrator/logs/joomla_scheduler.php
@@ -5,3 +5,24 @@
 
 #Fields: date	time	priority	message
 2026-08-22	09:49:50	INFO	Successfully finished task#01 in 0.02 (net 0.02) seconds.
+2026-08-24	02:16:54	INFO	Running task#02 'Session GC'.
+2026-08-24	02:16:54	INFO	Task> SessionGC end
+2026-08-24	02:16:54	INFO	Successfully finished task#02 in 0.01 (net 0.01) seconds.
+2026-08-28	01:55:55	INFO	Running task#03 'Update Notification'.
+2026-08-28	01:56:02	INFO	Task> Could not instantiate mail function.
+2026-08-28	01:56:02	INFO	Task> Update Notification end
+2026-08-28	01:56:03	INFO	Successfully finished task#03 in 7.26 (net 7.27) seconds.
+2026-08-28	02:00:54	INFO	Running task#02 'Session GC'.
+2026-08-28	02:00:54	INFO	Task> SessionGC end
+2026-08-28	02:00:54	INFO	Successfully finished task#02 in 0.01 (net 0.01) seconds.
+2026-09-03	01:21:11	INFO	Running task#03 'Update Notification'.
+2026-09-03	01:21:19	INFO	Task> Could not instantiate mail function.
+2026-09-03	01:21:19	INFO	Task> Update Notification end
+2026-09-03	01:21:19	INFO	Successfully finished task#03 in 7.95 (net 7.96) seconds.
+2026-09-03	01:26:19	INFO	Running task#02 'Session GC'.
+2026-09-03	01:26:19	INFO	Task> SessionGC end
+2026-09-03	01:26:19	INFO	Successfully finished task#02 in 0.01 (net 0.01) seconds.
+2026-09-04	02:43:09	INFO	Running task#03 'Update Notification'.
+2026-09-04	02:43:16	INFO	Task> Could not instantiate mail function.
+2026-09-04	02:43:16	INFO	Task> Update Notification end
+2026-09-04	02:43:16	INFO	Successfully finished task#03 in 7.21 (net 7.23) seconds.
